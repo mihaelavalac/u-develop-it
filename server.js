7f52688cdc617e155c73a4f5e1c250155a4eaaec
Mihaela Valac <mvalac@student.cscc.edu>
2022-01-17 10:03:19 -0500

    Created the connection to the election database.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express'); //Import express.
+const mysql = require('mysql2');  //Import mysql package from node_modules.
+const express = require('express'); //Import express from node_modules.
 //The PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,10 +8,18 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-
-
-
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
